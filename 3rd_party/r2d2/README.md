52a8bb312040bfbf5c2be02ac5d40ce3f0142026
yoffie <huaizhang.syf@alibaba-inc.com>
2021-01-14 13:08:48 +0800

    Add 3rd method R2D2.

---
+++ 3rd_party/r2d2/README.md
@@ -0,0 +1,2 @@
+clone the code from https://github.com/naver/r2d2
+copy these files into the project root dir, run run_exp.sh to extract features using r2d2.
---
+++ 3rd_party/r2d2/run_exp.sh
@@ -0,0 +1,5 @@
+
+python extract_hpatches.py --model models/r2d2_WASF_N16.pt \
+	--top-k 500 --max-scale 1 --min-scale 1  --max-size 10000 --min-size 10 \
+	--in_dir /path_to/hpatches_seq_resize/
+
